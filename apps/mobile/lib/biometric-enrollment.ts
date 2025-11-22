76548778564397b412a588ba575dd46efaa0f30e
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 04:38:52 -0800

    fix: Add getBiometricStatus to stub

--- apps/mobile/lib/biometric-enrollment.ts
+++ apps/mobile/lib/biometric-enrollment.ts
@@ -1,3 +1,4 @@
 export const enableBiometricFromSettings = async () => { return true; };
 export const disableBiometrics = async () => { return true; };
 export const isBiometricEnrolled = async () => { return false; };
+export const getBiometricStatus = async () => { return { isEnrolled: false, isEnabled: false }; };
