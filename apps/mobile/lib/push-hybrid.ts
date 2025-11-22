e6d17d4eb5bb5a5488bde75a829e1a026377e9f5
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 05:13:32 -0800

    fix: Add all missing exports to push stub

--- apps/mobile/lib/push-hybrid.ts
+++ apps/mobile/lib/push-hybrid.ts
@@ -1,1 +1,7 @@
 export const registerForPushNotificationsAsync = async () => { return null; };
+export const getPushToken = async () => { return null; };
+export const setupForegroundNotificationHandler = () => {};
+export const setupNotificationResponseHandler = () => {};
+export const setupTokenRefreshListener = () => {};
+export const unregisterForPushNotificationsAsync = async () => {};
+export const arePushNotificationsEnabled = async () => { return false; };
