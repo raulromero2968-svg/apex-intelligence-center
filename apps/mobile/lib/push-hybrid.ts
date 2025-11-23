099bd9147eab31f6f7c78bcb83643db7c494029d
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 16:43:50 -0800

    fix: Make push stubs accept arguments

--- apps/mobile/lib/push-hybrid.ts
+++ apps/mobile/lib/push-hybrid.ts
@@ -1,9 +1,9 @@
 
 export const registerForPushNotificationsAsync = async () => { return null; };
 export const getPushToken = async () => { return { token: null }; };
-export const setupForegroundNotificationHandler = () => {};
-export const setupNotificationResponseHandler = () => {};
-export const setupTokenRefreshListener = () => {};
+export const setupForegroundNotificationHandler = (...args: any[]) => {};
+export const setupNotificationResponseHandler = (...args: any[]) => {};
+export const setupTokenRefreshListener = (...args: any[]) => {};
 export const unregisterForPushNotificationsAsync = async () => {};
 export const arePushNotificationsEnabled = async () => { return false; };
 
