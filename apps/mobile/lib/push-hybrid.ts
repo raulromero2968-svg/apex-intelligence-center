8bd15ae5cd27e9b17e7302232b07d30bc270c40e
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 16:54:44 -0800

    fix: Make setupTokenRefreshListener return a cleanup function

--- apps/mobile/lib/push-hybrid.ts
+++ apps/mobile/lib/push-hybrid.ts
@@ -1,10 +1,11 @@
 
 export const registerForPushNotificationsAsync = async () => { return null; };
 export const getPushToken = async () => { return { token: null }; };
-// Explicitly define the callback signature
-export const setupForegroundNotificationHandler = (callback: (notification: any) => void) => { return { remove: () => {} }; };
-export const setupNotificationResponseHandler = (callback: (response: any) => void) => { return { remove: () => {} }; };
-export const setupTokenRefreshListener = (callback: (token: string) => void) => { return { remove: () => {} }; };
+// These return objects with .remove()
+export const setupForegroundNotificationHandler = (...args: any[]) => { return { remove: () => {} }; };
+export const setupNotificationResponseHandler = (...args: any[]) => { return { remove: () => {} }; };
+// This one is called directly as a function, so it must return a function
+export const setupTokenRefreshListener = (...args: any[]) => { return () => {}; };
 export const unregisterForPushNotificationsAsync = async () => {};
 export const arePushNotificationsEnabled = async () => { return false; };
 
