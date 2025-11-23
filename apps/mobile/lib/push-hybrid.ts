bf3d30ada68fe836ffbb6687a2df6402a4d9eafe
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 16:41:06 -0800

    fix: Update getPushToken stub to return object

--- apps/mobile/lib/push-hybrid.ts
+++ apps/mobile/lib/push-hybrid.ts
@@ -1,7 +1,9 @@
+
 export const registerForPushNotificationsAsync = async () => { return null; };
-export const getPushToken = async () => { return null; };
+export const getPushToken = async () => { return { token: null }; };
 export const setupForegroundNotificationHandler = () => {};
 export const setupNotificationResponseHandler = () => {};
 export const setupTokenRefreshListener = () => {};
 export const unregisterForPushNotificationsAsync = async () => {};
 export const arePushNotificationsEnabled = async () => { return false; };
+
