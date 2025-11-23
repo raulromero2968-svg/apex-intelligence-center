dfb180bc9551cc27c616c343c4c24441e63e7115
raulromero2968-svg <238164850+raulromero2968-svg@users.noreply.github.com>
2025-11-22 20:43:42 -0500

    fix: Convert Redis quit() promises to void using .then()
    Changed from type assertion to .then(() => {}) to properly convert
    Promise<'OK'> to Promise<void>. TypeScript was rejecting the direct
    cast as potentially unsafe.

--- apps/web/lib/pubsub.ts
+++ apps/web/lib/pubsub.ts
@@ -162,12 +162,12 @@ class PubSubPool {
     const promises: Promise<void>[] = [];
 
     if (this.publisher) {
-      promises.push(this.publisher.quit() as Promise<void>);
+      promises.push(this.publisher.quit().then(() => {}));
       this.publisher = null;
     }
 
     if (this.subscriber) {
-      promises.push(this.subscriber.quit() as Promise<void>);
+      promises.push(this.subscriber.quit().then(() => {}));
       this.subscriber = null;
     }
 
