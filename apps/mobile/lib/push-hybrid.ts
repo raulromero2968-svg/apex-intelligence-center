356bce83f5e1bc7a1efd751f65e336dc0f0a3c48
raulromero2968-svg <raulromero2968@gmail.com>
2025-11-22 16:47:31 -0800

    fix: Add explicit types to push stub callbacks

--- apps/mobile/lib/push-hybrid.ts
+++ apps/mobile/lib/push-hybrid.ts
@@ -1,9 +1,9 @@
 
 export const registerForPushNotificationsAsync = async () => { return null; };
 export const getPushToken = async () => { return { token: null }; };
-export const setupForegroundNotificationHandler = (...args: any[]) => {};
-export const setupNotificationResponseHandler = (...args: any[]) => {};
-export const setupTokenRefreshListener = (...args: any[]) => {};
+export const setupForegroundNotificationHandler = (callback: (notification: any) => void) => {};
+export const setupNotificationResponseHandler = (callback: (response: any) => void) => {};
+export const setupTokenRefreshListener = (callback: (token: string) => void) => {};
 export const unregisterForPushNotificationsAsync = async () => {};
 export const arePushNotificationsEnabled = async () => { return false; };
 
